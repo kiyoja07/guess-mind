1d90a8246fd6288e44fe4eaf1d12389f6bf97bff
YongJaeKim <kiyoja07@gmail.com>
2021-02-23 17:59:26 +0900

    6.5 Game Programming part Three

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # guess-mind
 Realtime Drawing Game built with SocketIO, Gulp and Node
 
-6.4 Game Programming part Two
+6.5 Game Programming part Three
--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,7 +1,12 @@
 import { handleNewMessage } from "./chat";
 import { handelDisconnected, handleNewUser } from "./notifications";
 import { handleBeganPath, handleFilled, handleStrokedPath } from "./paint";
-import { handleGameStarted, handlePlayerUpdate } from "./players";
+import {
+  handelGameEnded,
+  handleGameStarted,
+  handleLeaderNotif,
+  handlePlayerUpdate,
+} from "./players";
 
 let socket = null;
 
@@ -10,12 +15,14 @@ export const getSocket = () => socket;
 export const initSockets = (aSocket) => {
   const { events } = window;
   socket = aSocket;
-  aSocket.on(events.newUser, handleNewUser);
-  aSocket.on(events.disconnected, handelDisconnected);
-  aSocket.on(events.newMsg, handleNewMessage);
-  aSocket.on(events.beganPath, handleBeganPath);
-  aSocket.on(events.strokedPath, handleStrokedPath);
-  aSocket.on(events.filled, handleFilled);
-  aSocket.on(events.playerUpdate, handlePlayerUpdate);
-  aSocket.on(events.gameStarted, handleGameStarted);
+  socket.on(events.newUser, handleNewUser);
+  socket.on(events.disconnected, handelDisconnected);
+  socket.on(events.newMsg, handleNewMessage);
+  socket.on(events.beganPath, handleBeganPath);
+  socket.on(events.strokedPath, handleStrokedPath);
+  socket.on(events.filled, handleFilled);
+  socket.on(events.playerUpdate, handlePlayerUpdate);
+  socket.on(events.gameStarted, handleGameStarted);
+  socket.on(events.leaderNotif, handleLeaderNotif);
+  socket.on(events.gameEnded, handelGameEnded);
 };
